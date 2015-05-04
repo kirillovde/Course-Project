cd734700d9646520b2fe190f0766a0ca415919a4
kirillovde <kirillovde@gmail.com>
2015-05-04 14:03:42 +0300

    reading files remade

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,31 @@
-library(data.table)
+#library(data.table)
 
-# reading files
-features<- fread("features.txt")
-activity_labels<- fread("activity_labels.txt")
-subject_test<- fread("test/subject_test.txt")
-subject_train<- fread("train/subject_train.txt")
-y_test<- fread("test/y_test.txt")
-y_train<- fread("train/y_train.txt")
-# i had to stick with setDT(read.table()) because fread() doesn't work correctly
-X_test<- setDT(read.table("test/X_test.txt", colClasses = "numeric"))
-X_train<- setDT(read.table("train/X_train.txt", colClasses = "numeric"))
+files <- c("test/y_test.txt", "train/y_train.txt",
+           "test/subject_test.txt", "train/subject_train.txt",
+           "test/X_test.txt", "train/X_train.txt",
+           "features.txt", "activity_labels.txt")
 
-#binding test and train data
-X<- rbind(X_test, X_train)
-y<- rbind(y_test, y_train)
-subject<- rbind(subject_test, subject_train)
+X <- rbindlist(lapply(files[5:6], read.table, colClasses = "numeric"))
+y <- rbindlist(lapply(files[1:2], fread))
+subject <- rbindlist(lapply(files[3:4], fread))
+features<- fread(files[7])
+activity_labels <- fread(files[8])
 
-#binding all data into one dataset
-X<- cbind(y, subject, X)
+# binding all data into one dataset
+X[, `:=` (Activity = y[[1]], Subject = subject[[1]])]
+setcolorder(X, c(562, 563, 1:561))
+setnames(X, c("Activity", "Subject", features[[2]]))
+
 # subsetting only mean and std columns
+X<- X[, like(colnames(X), "(.*)Activity|Subject|mean|std(.*)"), with = F]
 setkey(X, Activity)
 
 # adding descriptive column names
-ActivityLabel<- merge(y, activity_labels, by = "V1")
-X<- cbind(ActivityLabel[,V2], X)
-X[,`:=`(Activity = V1, V1= NULL)]
+ActivityLabel <- merge(y, activity_labels, by = "V1")
+X <- cbind(ActivityLabel[, V2], X)
+X[, `:=`(Activity = V1, V1 = NULL)]
 setkey(X, Activity, Subject)
 
 # creating tidy dataset in .txt file
-write.table(X[,lapply(.SD, mean), by = c("Activity", "Subject")], "./tidyset.txt", row.name=F)
-
+write.table(X[, lapply(.SD, mean), by = c("Activity", "Subject")], "./tidyset.txt", 
+            row.name = F) 
